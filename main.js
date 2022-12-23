a292f77577b4a371452b6caf03637dcb4be8a1d6
allanteone <allanteone@gmail.com>
2022-12-23 16:08:53 +0200

    adds getDigitsSum function

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-//commit
+function numberValidation(number) {
+    number = typeof(number) == "boolean" ? "boolean" : number;
+
+        return isNaN(number) ? false : true;
+}
+
+function getDigitsSum(number) {
+    let res = 0;
+
+    if (!numberValidation(number)) {
+        return -1;
+    }
+
+    number = Math.trunc(+number);
+    number = number >= 0 ? number : -number;
+
+    while (number > 0) {
+        res += number % 10;
+        number = Math.trunc(number / 10);
+    }
+
+    return res;
+}
+
+
+
+
+
+
+getDigitsSum(-1234.456);

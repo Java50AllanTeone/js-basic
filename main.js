80298466bf59c4eaf682de418f530bdf9e5622f7
allanteone <allanteone@gmail.com>
2022-12-26 01:08:25 +0200

    fixes myToString function and adds convertFraction

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function myParseInt(str, base) {
     const isNegative = str[0] == '-';
     let res = 0;
 
-    for (let i = isNegative ? 1 : 0; i < str.length; i++) {  
+    for (let i = isNegative ? 1 : 0; i < str.length; i++) {
         let code = getCode(str[i], base);
 
-        if (isNaN(code)) {    
+        if (isNaN(code)) {
             res = i > 0 ? res : code;
             break;
         }
-        res =  res * base + code;
+        res = res * base + code;
     }
 
     return isNegative ? -res : res;
@@ -23,14 +23,29 @@ function getCode(symbol, base) {
     const codeA = 'a'.charCodeAt();
     const symbolCode = symbol.charCodeAt() - codeA + 10;
 
-    return symbol < 'a' || symbol > 'z' || symbolCode >= base ? +symbol : symbolCode;   
+    return symbol < 'a' || symbol > 'z' || symbolCode >= base ? +symbol : symbolCode;
     //verifies that symbol does not overstep the base numeric interval
 }
 
 function myToString(number, base) {
-    let res = '';
     base = base || 10;
-    number = Math.trunc(Math.abs(number));
+    const isNegative = number < 0;
+    number = Math.abs(number);
+
+    let res = convertNumber(Math.trunc(number), base);
+
+    if (number % 1) {
+        const strNumbers = ("" + number).split(".");
+        const fractNumber = myParseInt(strNumbers[1]);
+
+        res = `${res}.${convertNumber(fractNumber, base)}`;
+    }
+
+    return isNegative ? `-${res}` : res;
+}
+
+function convertNumber(number, base) {
+    let res = "";
 
     do {
         const digit = number % base;
@@ -42,11 +57,28 @@ function myToString(number, base) {
     return res;
 }
 
+function convertFraction(number, base) {
+    let res = '';
+
+    while (number) {        //method for correct converting fractional part to any numeric system as Number.toString()
+                            //works correctly only with converting from decimal to binary
+        number *= base;
+        if (number >= 1) {
+            res += Math.trunc(number);
+            number = number % 1 == 0 ? 0 : number - Math.trunc(number);
+        } else {
+            res += "0";
+        }
+    }
+    
+    return res;
+}
+
 function getSymbol(digit) {
     const codeA = 'a'.charCodeAt();
     let symbol = digit < 10 ? "" + digit : String.fromCharCode(digit - 10 + codeA);
 
-   return symbol;
+    return symbol;
 }
 
 
@@ -57,7 +89,7 @@ function testFn(fn, arg, arg2, expRes) {
     if (isNaN(expRes)) {
         return isNaN(fn(arg, arg2)) ? passMsg : errMsg;
     }
-    
+
     return fn(arg, arg2) === expRes ? passMsg : errMsg;
 }
 
@@ -68,16 +100,18 @@ console.log(testFn(myParseInt, "zz", 16, NaN));
 console.log(testFn(myParseInt, "123m", null, parseInt("123m")));
 console.log(testFn(myParseInt, "123.35", null, parseInt("123.35")));
 console.log(testFn(myParseInt, "-123", null, parseInt("-123")));
-console.log("");
+
+// let n = 0.816;
+// console.log(n.toString(3));
+// console.log(myToString(n, 3));
 
 
-
-// let num = 990500;
-// console.log(testFn(myToString, num, null, num.toString()));
-// console.log(testFn(myToString, num, 36, num.toString(36)));
-// num = 123.45;
-// console.log(testFn(myToString, num, 16, num.toString(16)));
-// console.log(testFn(myToString, num, null, num.toString()));
-// num = -123;
-// console.log(testFn(myToString, num, null, num.toString()));
+let num = 990500;
+console.log(testFn(myToString, num, null, num.toString()));
+console.log(testFn(myToString, num, 36, num.toString(36)));
+num = 123.45;
+console.log(testFn(myToString, num, 16, num.toString(16)));
+console.log(testFn(myToString, num, null, num.toString()));
+num = -123;
+console.log(testFn(myToString, num, null, num.toString()));
 

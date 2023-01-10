4226a8f86f14d12c9a19edd21211e0bce7358c0a
allanteone <allanteone@gmail.com>
2023-01-10 21:40:47 +0200

    adds getHtmlUl function

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-//commit
+
+function getHtmlUl(array) {
+    const arrLi = [];
+
+    if (!Array.isArray(array) || !isValid(array)) {
+        return 'array is not valid';
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        const classStr = array[i] == 0 ? 'white' : 'black';
+        arrLi.push(`<li class="item_class"><div class="${classStr}"> </div></li>`);
+    }
+    
+    return `<ul class="list_class">${arrLi.join('')}</ul>`;
+}
+
+function isValid(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== 0 && array[i] !== 1) {
+            return false;
+        }
+    }
+    return true;
+}

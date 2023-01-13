7c3e62e1766801c38569d326e62cee44f3037bb3
allanteone <allanteone@gmail.com>
2023-01-13 21:20:44 +0200

    adds coloringString, getNumbersWithDigitsAmount fs

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-//commit
+
+function coloringString(str1, str2) {
+    if (!isStringsValid(str1, str2)) {
+        console.log("Wrong arguments");
+        return undefined;
+    }
+
+    str2Arr = Array.from(str2);
+
+    return str2Arr.map(function(char, index)  {
+        return str1.indexOf(char, index) == index ? "green" : str1.indexOf(char) >= 0 ? "yellow" : "red";
+    });
+}
+
+function isStringsValid(arg1, arg2) {
+    return typeof arg1 == "string" && typeof arg2 == "string" && arg1.length === arg2.length;
+}
+
+
+function getNumbersWithDigitsAmount(digitsAmount, array) {
+
+    return array.filter(function(number) {
+        return Math.abs(number).toString().length === digitsAmount;
+    });
+}

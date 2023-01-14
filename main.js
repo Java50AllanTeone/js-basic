8307c38584e4bf5bb952017fe7343ad9ed3f966b
allanteone <allanteone@gmail.com>
2023-01-14 20:03:13 +0200

    adds evenOddSort, oddEvenSort functions

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-//commit
+
+function evenOddSort(array) {
+    const res = array.sort(function(arg1, arg2) {
+        const result =  arg1 % 2 == 0 ? -1 : 1;
+
+        if (result < 0) {
+            return arg2 % 2 == 0 ? 0 : -1;
+        }
+    });
+    return res;
+}
+
+function oddEvenSort(array) {
+    const res = array.sort(function(arg1, arg2) {
+        const result = arg1 % 2 != 0 ? -1 : 1;
+
+        if (result < 0) {
+            return arg2 % 2 != 0 ? 0 : -1;
+        }
+    });
+    return res;
+}
+
+console.log(evenOddSort([20, -10, 333, 1000, 552, 7, -7]));
+console.log(oddEvenSort([20, -10, 333, 1000, 552, 7, -7]));

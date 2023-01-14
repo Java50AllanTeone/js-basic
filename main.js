495daf0818609e1a8535e67a43cb118838f3dd44
allanteone <allanteone@gmail.com>
2023-01-14 23:05:25 +0200

    adds evenAscOddDesc function

--- main.js
+++ main.js
@@ -21,5 +21,22 @@ function oddEvenSort(array) {
     return res;
 }
 
+function evenAscOddDesc(array) {
+    const res = array.sort(function(arg1, arg2) {
+        let result;
+
+        if (arg1 % 2 == 0 && arg2 % 2 == 0) {
+            result = arg1 - arg2;
+        } else if (arg1 % 2 != 0 && arg2 % 2 != 0) {
+            result = arg2 - arg1;
+        } else {
+            result = arg1 % 2 == 0 ? -1 : 1;
+        }
+        return result;
+    });
+    return res;
+
+}
+
 console.log(evenOddSort([20, -10, 333, 1000, 552, 7, -7]));
-console.log(oddEvenSort([20, -10, 333, 1000, 552, 7, -7]));
+console.log(evenAscOddDesc([20, -10, 333, 1000, 552, 7, -7]));

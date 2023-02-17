25f83a9150ec19888dd4338b8a063e4dd6eac4e2
allanteone <allanteone@gmail.com>
2023-02-17 13:57:14 +0200

    hw-21 solution

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const employee3 = employee1;
 //console.log(`employee3==employee1 is ${employee3 == employee1}`)
 employee3.salary = 20000;
 //console.log(`employee1 salary = ${employee1.salary} `)
-function createEmployee(id, name, birthYear, salary, city, country) {
+/*function createEmployee(id, name, birthYear, salary, city, country) {
     return {id, name, birthYear, salary, address: {city, country}}
 }
 const employees = [
@@ -19,14 +19,14 @@ const employees = [
     createEmployee(128, "Goga", 1993, 10000, "Tbilisi", "UK"),
     createEmployee(129, "Sasha", 2000, 25000, "Ramat Gan", "Israel"),
     createEmployee(130, "Victor", 2003, 10000, "Lod", "Israel")
-]
+]*/
 //const index = employees.indexOf(createEmployee(126, "Abraham", 1990, 13000, "London", "UK"))
-const index = employees.findIndex(function(empl) {
+/*const index = employees.findIndex(function(empl) {
     return empl.id === 126;
 })
 const employee = employees.find(function(empl) {
     return empl.id === 126;
-})
+})*/
 
 
 
@@ -40,7 +40,6 @@ let field = "salary";
 function displayFieldValue(employees, index, field) {
     console.log(employees[index][field]);
 }
-
 
 function computeMinMaxAvgSalary(employees) {
     const res =  employees.reduce((res, empl) => {
@@ -61,7 +60,7 @@ function displayValue(minMaxAvg, field) {
     console.log(`value of the field ${field} is ${minMaxAvg[field]}`)
 };
 
-const minMaxAvg = computeMinMaxAvgSalary(employees);
+//const minMaxAvg = computeMinMaxAvgSalary(employees);
 
 
 const strings = ["b", "xyz", "lmn", "xyz", "lmn", "xyz", "a"];
@@ -94,12 +93,6 @@ function stringComp(entry1, entry2) {
     return res;
 }
 
-//displayStringOccurrences(strings);
-
-
-
-
-//HW #19
 function getMostPopulatedCountry(employees) {       //returns country with most amount of employees
     
     const stringOccurrences = getStringOccurrences(employees.map(e => e.address.country));
@@ -114,7 +107,6 @@ function getMostPopulatedCountry(employees) {       //returns country with most 
     return res[0];
 }
 
-
 function getMostPupulatedCountries(employees, counter) {        //returns a given number (conter) of countries with most amount of employees
 
     const stringOccurrences = getStringOccurrences(employees.map(e => e.address.country));
@@ -123,7 +115,6 @@ function getMostPupulatedCountries(employees, counter) {        //returns a give
 
     return arrayOccurrences.slice(0, counter);
 }
-
 
 function isAnagram(word, anagram) {     //returns true if a given anagram is indeed an angram of a given word
     const wordArr = Array.from(word);
@@ -134,5 +125,78 @@ function isAnagram(word, anagram) {     //returns true if a given anagram is ind
     return wordArr.toString() === anagramArr.toString();
 }
 
-console.log(getMostPupulatedCountries(employees, 3));
-console.log(isAnagram("word", "drow"));
+// Employe structure and function createEmployee() taken from previous HW
+function createEmployee(id, name, birthYear, salary, city, country) {
+    return {id, name, birthYear, salary, address: {city, country}}
+}
+
+
+class Company {
+    #employees; //object key: <id value>, value: reference to Employee object
+
+    constructor() {
+        this.#employees = {};
+    }
+
+    addEmployee(empl) {
+        let res = true;
+
+        this.#employees[empl.id] ? res = false : this.#employees[empl.id] = empl;
+        return res;
+    }
+
+    removeEmployee(id) {
+        let res = true;
+
+        this.#employees[id] ? delete this.#employees[id] : res = false;;
+        return res;
+    }
+
+
+    getEmployeesCountry(country) {
+        const emplArr = Object.values(this.#employees);
+
+        return emplArr.filter(e => e.address.country === country);
+    }
+    
+    getEmployeesByAge(age) {
+        const emplArr = Object.values(this.#employees);
+        return emplArr.filter(e => new Date().getFullYear() - e.birthYear === age); 
+    }
+
+    getEmployeesBySalaries(salryFrom, salryTo) {
+        const emplArr = Object.values(this.#employees);
+
+        return emplArr.filter(e => {
+            let res;
+
+            if (salryFrom < 0 && salryTo < 0) {
+                res = e;
+            } else if (salryFrom < 0) {
+                res = e.salary <= salryTo;  
+            } else if (salryTo < 0) {
+                res = e.salary >= salryFrom;
+            } else {
+                res = e.salary >= salryFrom && e.salary <= salryTo;
+            }
+            
+            return res;
+        });
+    }
+}
+
+const comp = new Company();
+comp.addEmployee(createEmployee(123, "Vasya", 2000, 15000, "Lod", "Israel"));
+comp.addEmployee(createEmployee(124, "David", 1975, 15500, "Tel Aviv", "Israel"));
+comp.addEmployee(createEmployee(125, "Sara", 1985, 20000, "New York", "USA"));
+comp.addEmployee(createEmployee(126, "Abraham", 1990, 13000, "London", "UK"));
+comp.addEmployee(createEmployee(127, "Moshe", 2000, 15000, "Lod", "Israel"));
+comp.addEmployee(createEmployee(128, "Goga", 1993, 10000, "Tbilisi", "UK"));
+comp.addEmployee(createEmployee(129, "Sasha", 2000, 25000, "Ramat Gan", "Israel"));
+comp.addEmployee(createEmployee(130, "Victor", 2003, 10000, "Lod", "Israel"));
+
+
+console.log(comp);
+
+const arrCountry = comp.getEmployeesBySalaries(10000, 15000);
+console.log(arrCountry);

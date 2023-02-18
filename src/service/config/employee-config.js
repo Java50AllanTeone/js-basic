307a5cb880fd46da7bef9fe4f5b591d95de6b4b0
allanteone <allanteone@gmail.com>
2023-02-18 14:48:31 +0200

    removes code from the others hw's

--- src/service/config/employee-config.js
+++ src/service/config/employee-config.js
@@ -1,6 +1,10 @@
 export const employeeConfig = {
     midId: 100000000,
     maxId: 999999999,
+    minSalary: 5000,
+    maxSalary: 50000,
+    minYear: new Date().getFullYear() - 120,
+    maxYear: new Date().getFullYear() - 18,
     countries: {
         "Israel": ["Lod", "Tel Aviv", "Rehovot"],
         "USA": ["New York", "Boston", "Washington"]

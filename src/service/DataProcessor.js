5be9d40253ce561555dc338b3da7c003506e46e6
allanteone <allanteone@gmail.com>
2023-02-22 09:22:18 +0200

    final weather app

--- src/service/DataProcessor.js
+++ src/service/DataProcessor.js
@@ -1,30 +1,45 @@
 export class DataProcessor {
     #url;
     #cities;
-
-
     constructor(url, cities) {
         this.#url = url;
         this.#cities = cities;
     }
-
-
-    async getData(latitude, longitude) {
-        const responseFromServer =  await fetch(`${this.#url}&latitude=${latitude}&longitude=${longitude}`);
+    async #getData(actualUrl) {
+        const responseFromServer =
+            await fetch(actualUrl);
         return responseFromServer.json();
-    }
 
+
+    }
     async getTemperatureData(city, startDate, endDate, hourFrom, hourTo) {
-        const responseFromServer =  await fetch(`${this.#url}&latitude=${this.#cities[city].latitude}&longitude=${this.#cities[city].longitude}&start_date=${startDate}&end_date=${endDate}`);
-        const obj = await responseFromServer.json();
-        const arr = obj.hourly.time.map((str, index) => {
-            const date = str.split('T');
-            const hour = date[1].split(":");
-            return {date: date[0], hour: hour[0], temperature: obj.hourly.temperature_2m[index]}
-            
-        });
-        return arr.filter(obj => !(obj.date === startDate && obj.hour < hourFrom || obj.date === endDate && obj.hour > hourTo));
+        const latLong = this.#cities[city];
+        const actualUrl = this.#getActualUrl(latLong.latitude, latLong.longitude,
+            startDate, endDate);
+        const rawData = await this.#getData(actualUrl);
+        return processRawData(rawData, hourFrom, hourTo);
     }
+    #getActualUrl(latitude, longitude, startDate, endDate) {
+        return `${this.#url}&latitude=${latitude}&longitude=${longitude}&start_date=${startDate}&end_date=${endDate}`
+    }
+}
+function processRawData(rawData, hourFrom, hourTo) {
+    
+    const timeArray = getHoursElements(rawData.hourly.time, hourFrom, hourTo);
+    const temperatureArray =getHoursElements(rawData.hourly.temperature_2m, hourFrom, hourTo); ;
+    return timeArray.map((t, index) => {
+        const res = {};
+        const dateTime = t.split("T");
+        res.date = dateTime[0];
+        res.hour = dateTime[1];
+        res.temperature = temperatureArray[index];
+        return res;
 
-
+    })
+}
+function getHoursElements (array, hourFrom, hourTo) {
+    return array.filter((__, index) => {
+        const hour = index % 24;
+        return hour >= hourFrom && hour <= hourTo
+    } )
 }

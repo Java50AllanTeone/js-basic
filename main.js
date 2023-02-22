f8babcc2def9f6f195638a0bd91f82f4ee8e751f
allanteone <allanteone@gmail.com>
2023-02-22 20:17:40 +0200

    adds class Deferred

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-//commit
+class Deferred {
+    #functions = [];
+
+    constructor() {
+    }
+
+    then(fn) {
+        this.#functions.push(fn);
+    }
+
+    resolve(arg) {
+        this.#functions.forEach(fn => arg = fn(arg));
+    }
+}
+
+const d = new Deferred()
+d.then(function(res){ console.log("1 ", res); return "a"; });
+d.then(function(res){ console.log("2 ", res); return "b"; });
+d.then(function(res){ console.log("3 ", res); return "c"; });
+d.resolve('hello');
